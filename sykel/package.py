ca913ee8c813c06a1711357567bc237f865d3036
Øystein S. Haaland <oystein@beat.no>
2022-01-20 10:41:21 +0100

    Make docs of publish task in package module more generic

--- sykel/package.py
+++ sykel/package.py
@@ -27,23 +27,9 @@ def build(ctxt):
 @task
 def publish(ctxt, repository):
     """
-    Publish package and push to all remotes: --repository <pypi,gitlab>
+    Publish package and push to all remotes: --repository <>
     
-    The mandtory `--repository` option expects repositories to be configured in `~/.pypirc`:
-    
-        [distutils]
-        index-servers =
-            pypi
-            gitlab
-
-        [pypi]
-        username = __token__
-        password = pypi-<token>
-
-        [gitlab]
-        repository = https://gitlab.dev.beat.no/api/v4/projects/229/packages/pypi
-        username = __token__
-        password = <token>
+    The mandtory `--repository` option expects repositories to be configured in `~/.pypirc`.
     """
     run(f"twine upload --repository {repository} dist/*")
     run("git remote | xargs -L 1 -I remote git push remote")
